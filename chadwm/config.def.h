d0155e24618bf9844db17db2cff575090fbf1461
Francesco <28204054+FrancescoXD@users.noreply.github.com>
2021-08-01 01:39:29 +0200

    fixed termcmd

--- chadwm/config.def.h
+++ chadwm/config.def.h
@@ -129,7 +129,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
-static const char *termcmd[]  = {  "st", NULL }; // change this to your term
+static const char *termcmd[]  = {  "alacritty", NULL }; // change this to your term
 static const char *rofi[] = {"rofi", "-show", "drun", NULL };
 static const char *layoutmenu_cmd = "layoutmenu.sh";
 static const char *xi[] = {"xbacklight", "-inc", "7", NULL};
@@ -138,10 +138,7 @@ static const char *xd[] = {"xbacklight", "-dec", "7", NULL};
 static Key keys[] = {
     /* modifier                     key        function        argument */
     { MODKEY,                       XK_c,      spawn,          {.v = rofi } },
-
-    // if you dont use st my rm this and uncomment line below it!
-    { MODKEY,                       XK_Return, spawn,   SHCMD("~/.local/bin/./st_settings && st")}, 
- // { MODKEY,                       XK_Return, spawn,    {.v = term }}, 
+    { MODKEY,                       XK_Return, spawn,          {.v = termcmd }},
 
     {MODKEY | ControlMask, XK_u, spawn, SHCMD("maim | xclip -selection clipboard -t image/png")},
     {MODKEY, XK_u, spawn,   SHCMD("maim --select | xclip -selection clipboard -t image/png")},
