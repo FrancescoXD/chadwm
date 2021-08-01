c9649eeb5f014c530685421886256741f404c2f1
Francesco <28204054+FrancescoXD@users.noreply.github.com>
2021-08-01 02:42:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 mkdir $HOME/Wallpapers
 cp wallpapers/wall.jpg $HOME/Wallpapers
-mv chadwm/.dwm $HOME/
+cp -r chadwm/.dwm $HOME/
 chmod +x $HOME/.dwm/autostart
 chmod +x $HOME/.dwm/bar
-sudo mv chadwm/layoutmenu /usr/local/bin
+sudo cp chadwm/layoutmenu.sh /usr/local/bin
 cd chadwm
 cd dwm
 make
