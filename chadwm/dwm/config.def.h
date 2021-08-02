fa6294476f29ed56a2a902db126f1db8e949ffc5
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2021-08-03 00:58:23 +0200

    merged and updates

--- chadwm/dwm/config.def.h
+++ chadwm/dwm/config.def.h
@@ -132,15 +132,15 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = {  "st", NULL }; // change this to your term
 static const char *rofi[] = {"rofi", "-show", "drun", NULL };
-static const char *layoutmenu_cmd = "/home/sid/.dwm/layoutmenu.sh";
+static const char *layoutmenu_cmd = "/home/system64/.dwm/layoutmenu.sh";
 static const char *xi[] = {"xbacklight", "-inc", "7", NULL};
 static const char *xd[] = {"xbacklight", "-dec", "7", NULL};
-static const char *preview_path = "/home/sid/.dwm/tag_preview";
+static const char *preview_path = "/home/system64/.dwm/tag_preview";
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
     { MODKEY,                       XK_c,      spawn,          {.v = rofi } },
-    { MODKEY,                       XK_Return, spawn,    {.v = termcmd }}, 
+    { MODKEY,                       XK_Return, spawn,          {.v = termcmd }}, 
 
     {MODKEY | ControlMask, XK_u, spawn, SHCMD("maim | xclip -selection clipboard -t image/png")},
     {MODKEY, XK_u, spawn,   SHCMD("maim --select | xclip -selection clipboard -t image/png")},
