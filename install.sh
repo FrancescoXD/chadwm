5f7f0ca622e67004e075ae44a8e1b9bb0c670fb2
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2021-08-03 01:09:24 +0200

    fixed README and updated scritps

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ cp wallpapers/wall.jpg $HOME/Wallpapers
 cp -r chadwm/.dwm $HOME/
 chmod +x $HOME/.dwm/autostart
 chmod +x $HOME/.dwm/bar
-sudo cp chadwm/layoutmenu.sh /usr/local/bin
+chmod +x $HOME/.dwm/layoutmenu.sh
+chmod +x $HOME/.dwm/save_tagpreview
+chmod +x $HOME/.dwm/tag_preview
 cd chadwm
 cd dwm
 make
--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 # Install packages
-sudo pacman -S xorg-xsetroot python-pywal pacman-contrib
+sudo pacman -S xorg-xsetroot pacman-contrib
 
 # Install yay
 git clone https://aur.archlinux.org/yay.git
@@ -8,3 +8,9 @@ makepkg -si
 
 # Install AUR packages
 yay -S xmenu nerd-fonts-jetbrains-mono ttf-material-design-icons
+
+# Install st
+git clone https://github.com/siduck76/st
+cd st
+make
+sudo make install
