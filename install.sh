606bc47b7d0c1dc9a4b0d6f3feb801da32a2916c
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2021-08-03 01:34:36 +0200

    added nord xresources

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 mkdir $HOME/Wallpapers
+mkdir -p $HOME/Public/Xresources/nord
+cat nord-xresources/src/nord > $HOME/Public/Xresources/nord/.Xresources
 cp wallpapers/wall.jpg $HOME/Wallpapers
 cp -r chadwm/.dwm $HOME/
 chmod +x $HOME/.dwm/autostart
