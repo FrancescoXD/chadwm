a8532b6f5d4df4bd0a56ef16eaaddb1912e3f627
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2021-08-02 22:39:34 +0200

    added pacman_updates

--- chadwm/dwm/config.def.h
+++ chadwm/dwm/config.def.h
@@ -129,7 +129,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
-static const char *termcmd[]  = {  "alacritty", NULL }; // change this to your term
+static const char *termcmd[]  = {  "st", NULL }; // change this to your term
 static const char *rofi[] = {"rofi", "-show", "drun", NULL };
 static const char *layoutmenu_cmd = "layoutmenu.sh";
 static const char *xi[] = {"xbacklight", "-inc", "7", NULL};
