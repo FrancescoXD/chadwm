c05c9248920581cefc367faa469aa17720d0f0bb
Francesco <28204054+FrancescoXD@users.noreply.github.com>
2021-08-05 00:24:12 +0200

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,8 +1,15 @@
 # Install packages
 sudo pacman -S xorg-xsetroot xorg-xbacklight acpi pacman-contrib rofi feh
 
+# Install xmenu
+git clone https://aur.archlinux.org/xmenu.git
+cd xmenu
+makepkg -si
+cd ..
+
 # Install st (siduck76's build)
 git clone https://github.com/siduck76/st
 cd st
 make
 sudo make install
+cd ..
