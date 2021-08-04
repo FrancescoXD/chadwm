495840405632cdc1ad1f73815d462fb8478266a9
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2021-08-04 22:03:32 +0200

    fixed installer and added new background

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ cp -r fonts/* ~/.local/share/fonts
 mkdir $HOME/Wallpapers
 mkdir -p $HOME/Public/Xresources/nord
 cat nord-xresources/src/nord > $HOME/Public/Xresources/nord/.Xresources
-cp wallpapers/wall.jpg $HOME/Wallpapers
+cp -r wallpapers/* $HOME/Wallpapers
 cp -r chadwm/.dwm $HOME/
 chmod +x $HOME/.dwm/autostart
 chmod +x $HOME/.dwm/bar
