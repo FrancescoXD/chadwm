cf4d14645f35f47b95b2d1eb63b579a8924b9d7d
Francesco <28204054+FrancescoXD@users.noreply.github.com>
2021-08-05 12:57:57 +0200

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,12 @@
 # Install packages
 sudo pacman -S xorg-xsetroot xorg-xbacklight acpi pacman-contrib rofi feh
 
+# Install xmenu
+git clone https://aur.archlinux.org/xmenu.git
+cd xmenu
+makepkg -si
+cd ..
+
 # Install st (siduck76's build)
 echo "Do you want to have transparency in st? (It needs picom)"
 echo -n "=> "
